8fba99bde3b7b0c2feb3c8760a6edb6e6b5d93f1
kakalota <shadow_dragon@126.com>
2025-08-22 11:49:03 +0800

    update

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -4,13 +4,13 @@ import Link from 'next/link'
 import FramerWrapper from './animation/FramerWrapper'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquareTwitter, faSquareGithub } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faSquareEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 const SocialLinks = () => {
   const links = [
     { name: 'Twitter', link: 'https://x.com/Junexus_indie', icon: <FontAwesomeIcon icon={faSquareTwitter} /> },
     { name: 'Github', link: 'https://github.com/shadowDragons', icon: <FontAwesomeIcon icon={faSquareGithub} /> },
-    { name: 'Email', link: 'mailto:contact@sphrag.com', icon: <FontAwesomeIcon icon={faEnvelope} /> },
+    { name: 'Email', link: 'mailto:contact@sphrag.com', icon: <FontAwesomeIcon icon={faSquareEnvelope} /> },
   ]
   return (
     <>
